01cd5ef4a440f7cab46661592f012780017d2418
LiquidJaguar10 <83448848+LiquidJaguar-10@users.noreply.github.com>
2022-09-28 13:46:43 +0530

    Update random number.py

--- random number.py
+++ random number.py
@@ -1,8 +1,8 @@
-import random
-number = random.randint(0,100)
+import random 
+number = random.randint(0,100) #Chooses a number from 1 to 100
 guess = ''
-guess_count = 0
-guess_limit = 8
+guess_count = 0 
+guess_limit = 8 #Number of Guesses you have
 guesses_gone = False
 
 print('Welcome to the number guessing game')
@@ -10,18 +10,18 @@ a = input('Enter your name')
 print('Welcome to the game',a)
 
 
-while guess != number and not(guesses_gone):
-    if guess_limit > guess_count:
+while guess != number and not(guesses_gone): #Checks whether guesses are gone or not
+    if guess_limit > guess_count: #Checks whether the guesses are available or not
         guess = int(input('What\'s your guess(1-100)'))
         if guess > number:
             print('The number is lesser')
         elif guess < number:
             print('The number is greater')
-        guess_count += 1
+        guess_count += 1 #Increases guess count
     else:
-        guesses_gone = True
+        guesses_gone = True #Guesses are over
 
-if guesses_gone == True:
+if guesses_gone == True: 
     print('You lost')
 else:
     print('You win!')
